a05c1f6e79d239ea095ff4ebaf1ccac7d7e9f240
Andrew <andrew.venskij@bk.ru>
2025-11-20 07:27:39 +0100

    change userservice

--- src/main/java/project/dorce/usermanager/UserService.java
+++ src/main/java/project/dorce/usermanager/UserService.java
@@ -47,6 +47,7 @@ public class UserService {
             return null;
         }
         return authorizationHeader.substring(7);
+    }
     public User getUserByUsername(String username) {
         return userRepository.findByUsername(username);
     }
