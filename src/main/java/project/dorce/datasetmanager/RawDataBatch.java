b6205565adafa81bea51a75d7b281de165e4f6d4
Dominika Kalinowska <kalinooovska@gmail.com>
2025-11-24 22:01:10 +0100

    RawData - przeniesienie i modyfikacja klas

---
+++ src/main/java/project/dorce/datasetmanager/RawDataBatch.java
@@ -0,0 +1,49 @@
+package project.dorce.datasetmanager;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "raw_data_batches")
+@NoArgsConstructor
+public class RawDataBatch {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "raw_dataset_id", nullable = false)
+    @JsonIgnore
+    private RawDataset rawDataset;
+
+    @Column(nullable = false)
+    private String storageReference;
+
+    @Column
+    private String batchName;
+
+    @Column
+    private Long sizeInBytes;
+
+    @Column
+    private Integer entryCount;
+
+    @CreationTimestamp
+    private LocalDateTime appendedAt;
+
+    public RawDataBatch(RawDataset rawDataset, String storageReference, Long sizeInBytes, Integer entryCount) {
+        this.rawDataset = rawDataset;
+        this.storageReference = storageReference;
+        this.batchName = storageReference;
+        this.sizeInBytes = sizeInBytes;
+        this.entryCount = entryCount;
+    }
+}
