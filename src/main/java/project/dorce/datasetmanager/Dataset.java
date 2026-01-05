b2c372f7253df3968b002553107b7c2253e21570
Dominika Kalinowska <kalinooovska@gmail.com>
2026-01-05 01:53:03 +0100

    Refactor Catalog and Dataset entities to include publisher and creator agents; update User entity to associate with Agent

--- src/main/java/project/dorce/datasetmanager/Dataset.java
+++ src/main/java/project/dorce/datasetmanager/Dataset.java
@@ -1,14 +1,30 @@
 package project.dorce.datasetmanager;
 
-import jakarta.persistence.*;
-import lombok.Data;
-import project.dorce.usermanager.User;
-
+import java.time.LocalDate;
 import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import project.dorce.usermanager.Agent;
+import project.dorce.usermanager.DataOwner;
+import project.dorce.usermanager.DataSupplier;
+import project.dorce.usermanager.User;
 
 @Data
 @Entity
 @Table(name = "datasets")
+@NoArgsConstructor
 public class Dataset {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -21,7 +37,7 @@ public class Dataset {
     private String description;
 
     @ManyToOne
-    @JoinColumn(name = "owner_id", nullable = false)
+    @JoinColumn(name = "user_owner_id", nullable = false) 
     private User owner;
 
     @Column(nullable = false)
@@ -43,7 +59,31 @@ public class Dataset {
     @Enumerated(EnumType.STRING)
     private DatasetStatus status;
 
-    public Dataset() {}
+    @Column
+    private LocalDate issuedDate;
+
+    @Column
+    private LocalDate modifiedDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "catalog_id")
+    private Catalog catalog;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "publisher_agent_id")
+    private Agent publisherAgent;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "contact_point_agent_id")
+    private Agent contactPointAgent;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "data_supplier_id")
+    private DataSupplier dataSupplier;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "data_owner_id")
+    private DataOwner dataOwner;
 
     public Dataset(String title, String description, User owner, Schema schema, Boolean qualityControllable) {
         this.title = title;
--- src/main/java/project/dorce/usermanager/User.java
+++ src/main/java/project/dorce/usermanager/User.java
@@ -1,14 +1,24 @@
 package project.dorce.usermanager;
 
-import jakarta.persistence.*;
-import lombok.Data;
-
 import java.util.List;
 import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
 @Table(name = "users")
+@NoArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -22,13 +32,13 @@ public class User {
 
     @Column(unique = true, nullable = false)
     private String authToken;
-
+    
     @Column(nullable = false)
     private List<Role> roles;
 
-    public User(){
-
-    }
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "agent_id")
+    private Agent agent;
 
     public User(String username, String password, List<Role> roles, String authToken) {
         this.username = username;
