8faa814335f556846bc2a8f5f5f5747b08064c08
Dominika Kalinowska <kalinooovska@gmail.com>
2026-01-05 01:53:32 +0100

    Add repositories for Agent, AgentType, DataOwner, and DataSupplier entities

---
+++ src/main/java/project/dorce/usermanager/AgentRepository.java
@@ -0,0 +1,8 @@
+package project.dorce.usermanager;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AgentRepository extends JpaRepository<Agent, UUID> {
+}
